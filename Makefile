1f7dd84c3c15037f96437a5988e3491730889d74
oliver <olive.croomes@utexas.edu>
2014-05-10 16:22:07 -0500

    Okay, I know how to read in examples from this image and display a window. Now onto the harder stuff.

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 test:
 	g++ -o -g open_image open_image.cpp `Magick++-config --cppflags --cxxflags --ldflags --libs` 
 done:
-	g++ open_image.cpp -o open_image `Magick++-config --cppflags --cxxflags --ldflags --libs` 
+	g++ open_image.cpp -lGL -lglut -lGLU -o open_image `Magick++-config --cppflags --cxxflags --ldflags --libs` 
--- open_image.cpp
+++ open_image.cpp
@@ -1,21 +1,44 @@
 #include <cstdlib>
 #include <iostream>
-#include <Magick++.h>
 #include <stdio.h>
+
+//Need this for open gl stuff
+#include <GL/freeglut.h>
+
+//Need this for the image processing
+#include <Magick++.h>
 
 using namespace std;
 using namespace Magick;
 
-int main(int num_args,char** argv){
-		
+static void drawStuff(){
+
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();	
+
+}
+
+int main(int argc,char** argv){
+	
+	glutInit(&argc,argv);
+	glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+	glutInitWindowSize(1024,768);
+	glutInitWindowPosition(100,100);
+	glutCreateWindow("Tutorial 10");
+	
+	glutDisplayFunc(drawStuff);
+
+	glClearColor(0.0f,0.0f,0.0f,0.0f);
+
+	glutMainLoop();
+			
 	Image example_image;
 	
 	example_image.read("ExampleScreenShot.png");
-	
 	ColorRGB pixel = example_image.pixelColor(100,100);	
-	
+		
 	printf("%f %s", pixel.red() * 255.0,"Hopefully this is green\n");
-	printf("%d %d", example_image.width_ , example_image.height_ );
+	printf("%d %d", (int)example_image.size().width() ,(int) example_image.size().height() );
 	
 	
 	
